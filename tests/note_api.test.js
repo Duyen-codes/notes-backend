57ba13630a3f55e6439bf2b4ab69265c8379fb9a
Duyen Nguyen <hongduyen0705@gmail.com>
2022-09-30 12:24:46 +0300

    modified logger so it does not print to console in test mode, tests with async await

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -3,6 +3,28 @@ const supertest = require("supertest");
 const app = require("../app");
 
 const api = supertest(app);
+const Note = require("../models/note");
+
+const initialNotes = [
+  {
+    content: "HTML is easy",
+    date: new Date(),
+    important: false,
+  },
+  {
+    content: "Browser can execute only Javascript",
+    date: new Date(),
+    important: true,
+  },
+];
+
+beforeEach(async () => {
+  await Note.deleteMany({});
+  let noteObject = new Note(initialNotes[0]);
+  await noteObject.save();
+  noteObject = new Note(initialNotes[1]);
+  await noteObject.save();
+});
 
 test("notes are returned as json", async () => {
   await api
@@ -11,6 +33,25 @@ test("notes are returned as json", async () => {
     .expect("Content-Type", /application\/json/);
 }, 100000);
 
+test("all notes are returned", async () => {
+  const response = await api.get("/api/notes");
+
+  expect(response.body).toHaveLength(initialNotes.length);
+});
+
+test("the first note is about HTTP methods", async () => {
+  const response = await api.get("/api/notes");
+
+  expect(response.body[0].content).toBe("HTML is easy");
+});
 afterAll(() => {
   mongoose.connection.close();
+});
+
+test("a specific note is within the returned notes", async () => {
+  const response = await api.get("/api/notes");
+
+  const contents = response.body.map((r) => r.content);
+
+  expect(contents).toContain("Browser can execute only Javascript");
 });

b43666d4060ab1b220570c3fcc0015f264555000
Duyen-codes <hongduyen0705@gmail.com>
2022-10-23 15:54:24 +0300

    added userExtractor middleware, tested delete and put route

--- controllers/notes.js
+++ controllers/notes.js
@@ -27,71 +27,86 @@ notesRouter.get("/:id", async (request, response, next) => {
 //   return null;
 // };
 
-notesRouter.post("/", async (request, response, next) => {
-  const body = request.body;
-  // const token = getTokenFrom(request);
+notesRouter.post(
+  "/",
+  middleware.userExtractor,
+  async (request, response, next) => {
+    const body = request.body;
+    const user = await request.user;
+    // const token = getTokenFrom(request);
+    // const decodedToken = jwt.verify(request.token, process.env.SECRET);
+    // if (!decodedToken.id) {
+    //   return response.status(401).json({
+    //     error: "token missing or invalid",
+    //   });
+    // }
+    // const user = await User.findById(decodedToken.id);
+    // const user = await User.findById(body.userId);
 
-  const decodedToken = jwt.verify(request.token, process.env.SECRET);
-  if (!decodedToken.id) {
-    return response.status(401).json({
-      error: "token missing or invalid",
+    if (body.content === undefined) {
+      response.status(400).json({
+        error: "content missing",
+      });
+    }
+    const note = new Note({
+      content: body.content,
+      important: body.important === undefined ? false : body.important,
+      date: new Date(),
+      user: user._id,
     });
+    const savedNote = await note.save();
+    user.notes = user.notes.concat(savedNote._id);
+    await user.save();
+    response.status(201).json(savedNote);
   }
+);
 
-  const user = await User.findById(decodedToken.id);
-  // const user = await User.findById(body.userId);
+notesRouter.put(
+  "/:id",
+  middleware.userExtractor,
+  async (request, response, next) => {
+    console.log("request.body", request.body);
+    const { content, important } = request.body;
+    console.log("newNote content", content);
+    const user = request.user;
+    console.log("user in put route", user);
+    const userid = user._id;
+    const note = await Note.findById(request.params.id);
 
-  if (body.content === undefined) {
-    response.status(400).json({
-      error: "content missing",
-    });
+    console.log("note to change in put route", note);
+    const newNote = {
+      content,
+      important,
+    };
+
+    if (note.user.toString() === userid.toString()) {
+      const updatedNote = await Note.findByIdAndUpdate(
+        request.params.id,
+        newNote,
+        {
+          new: true,
+          runValidators: true,
+          context: "query",
+        }
+      );
+      response.status(200).json(updatedNote);
+    }
   }
+);
 
-  const note = new Note({
-    content: body.content,
-    important: body.important === undefined ? false : body.important,
-    date: new Date(),
-    user: user._id,
-  });
-  const savedNote = await note.save();
-  user.notes = user.notes.concat(savedNote._id);
-  await user.save();
-  response.status(201).json(savedNote);
-});
+notesRouter.delete(
+  "/:id",
+  middleware.userExtractor,
+  async (request, response, next) => {
+    const note = Note.findById(request.params.id);
+    const user = request.user;
+    const userid = user._id;
 
-notesRouter.put("/:id", (request, response, next) => {
-  const { content, important } = request.body;
-
-  const note = {
-    content,
-    important,
-  };
-
-  Note.findByIdAndUpdate(request.params.id, note, {
-    new: true,
-    runValidators: true,
-    context: "query",
-  })
-    .then((updatedNote) => {
-      response.json(updatedNote);
-    })
-    .catch((error) => next(error));
-});
-
-notesRouter.delete("/:id", async (request, response, next) => {
-  const body = request.body;
-  const decodedToken = jwt.verify(request.token, process.env.SECRET);
-
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: "token missing or invalid" });
+    if (note.user.toString() === userid.toString()) {
+      await Note.findByIdAndRemove(request.params.id);
+      response.status(204).end();
+    }
   }
-  const user = await User.findById(decodedToken.id);
-  const userid = user._id;
-  const note = Note.findById(request.params.id);
-  if (note.user.toString() === userid.toString()) {
-    await Note.findByIdAndRemove(request.params.id);
-    response.status(204).end();
-  }
-});
+);
 
 module.exports = notesRouter;
--- utils/middleware.js
+++ utils/middleware.js
@@ -1,4 +1,6 @@
 const logger = require("./logger");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
 
 const requestLogger = (request, response, next) => {
   logger.info("Request method: ", request.method);
@@ -18,6 +20,20 @@ const tokenExtractor = (request, response, next) => {
   const authorization = request.get("authorization");
   if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
     request.token = authorization.substring(7);
+  }
+  next();
+};
+
+const userExtractor = async (request, response, next) => {
+  if (!request.token) {
+    return response.status(401).json({ error: "token missing" });
+  }
+  const decodedToken = jwt.verify(request.token, process.env.SECRET);
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: "token invalid" });
+  }
+  if (decodedToken) {
+    request.user = await User.findById(decodedToken.id);
   }
   next();
 };
@@ -47,5 +63,6 @@ module.exports = {
   requestLogger,
   unknownEndpoint,
   tokenExtractor,
+  userExtractor,
   errorHandler,
 };
